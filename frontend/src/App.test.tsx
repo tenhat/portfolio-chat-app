a956df5512a52e0d37b0535946487734d42a6e21
tenhat <tenjuhattori@gmail.com>
2024-01-24 10:58:37 +0900

    App.test.tsx: 作成

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { configure, render, screen } from '@testing-library/react'
 import App from './App'
+import { configureStore } from '@reduxjs/toolkit'
+import MessageReducer from './silces/MessageSlice'
+import SentMessagesReducer from './silces/SentMessagesSlice'
+import UserReducer from './silces/UserSlice'
+import { Provider } from 'react-redux'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+test('renders the chat component', () => {
+  const store = configureStore({
+    reducer: {
+      message: MessageReducer,
+      sentMessages: SentMessagesReducer,
+      user: UserReducer
+    }
+  })
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+  const chatElement = screen.getByText(/チャットを始めよう！/i)
+  expect(chatElement).toBeInTheDocument()
 })
